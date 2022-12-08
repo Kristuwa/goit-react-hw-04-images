58dce63ec583ed68ec3981b5ac1a037c98578bef
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2022-12-08 17:19:44 +0200

    Update Modal.js

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -32,48 +32,6 @@ export const Modal = ({ url, onModalClose }) => {
   );
 };
 
-// export class Modal extends Component {
-//   static defaultProps = {
-//     url: '',
-//     onModalClose: null,
-//   };
-
-//   static propTypes = {
-//     url: PropTypes.string.isRequired,
-//     onModalClose: PropTypes.func.isRequired,
-//   };
-
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.onCloseEsc);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.onCloseEsc);
-//   }
-
-//   onCloseEsc = e => {
-//     if (e.code === 'Escape') {
-//       this.props.onModalClose();
-//     }
-//   };
-
-//   onCloseBackdrop = e => {
-//     if (e.currentTarget === e.target) {
-//       this.props.onModalClose();
-//     }
-//   };
-
-//   render() {
-//     const { url } = this.props;
-//     return (
-//       <Overlay onClick={this.onCloseBackdrop}>
-//         <ModalContainer>
-//           <img src={url} alt={url} width="900" height="680" />
-//         </ModalContainer>
-//       </Overlay>
-//     );
-//   }
-// }
 
 Modal.propTypes = {
   url: PropTypes.string.isRequired,
